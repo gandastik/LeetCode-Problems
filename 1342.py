5cca437f538414a140aa5b4f07337f7dd38144b7
gandastik <gandastik@gmail.com>
2021-01-21 14:24:42 +0700

    change discription 1342

--- 1342.py
+++ 1342.py
@@ -1,4 +1,4 @@
-#Jan 21, 2021 - Given a non-negative integer num, return the number of steps to reduce it to zero. 
+# Given a non-negative integer num, return the number of steps to reduce it to zero. 
 # If the current number is even, you have to divide it by 2, otherwise, you have to subtract 1 from it.
 def stepsToZero(num: int) -> int:
     count = 0
