a963e00338b2c93ca6e03aab0853a2710f2df441
gandastik <gandastik@gmail.com>
2021-04-04 13:43:20 +0700

    solved 412

---
+++ 412.py
@@ -0,0 +1,16 @@
+from typing import List
+def fizzBuzz(n: int) -> List[str]:
+    ret = []
+    for i in range(1,n+1):
+        if(i % 3 == 0 and i % 5 == 0):
+            ret.append("FizzBuzz")
+        elif(i % 3 == 0):
+            ret.append("Fizz")
+        elif(i % 5 == 0):
+            ret.append("Buzz")
+        else:
+            ret.append(str(i))
+    return ret
+
+n = int(input())
+print(fizzBuzz(n))
