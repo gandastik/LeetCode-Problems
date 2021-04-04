9c7cb31d85427000e01e2f10f8679c08a4e29b2c
gandastik <gandastik@gmail.com>
2021-04-04 13:43:40 +0700

    solved 412

--- 412.py
+++ 412.py
@@ -1,3 +1,5 @@
+#412. Write a program that outputs the string representation of numbers from 1 to n.
+# But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
 from typing import List
 def fizzBuzz(n: int) -> List[str]:
     ret = []
